fbbf4c1f2625e510a8348feb5e30d552e4bcc09c
Manas Gupta <73732686+guptamanas@users.noreply.github.com>
2022-12-20 12:16:40 +0530

    Add files via upload

---
+++ mathematics.js
@@ -0,0 +1,9 @@
+const math=require('mathjs');//importung the internal module to perform complex mathematical operations
+var x=10;
+var y=20;
+var z=(x+y)*30;//simple
+console.log(z);
+
+console.log(math.pow(3,3));
+console.log(math.sqrt(4));
+//if mathjs module is not there then use-> ( npm install mathjs )  to install mathjs module
